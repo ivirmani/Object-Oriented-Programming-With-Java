6bcd3cb37866d47cfe366fab79a095dc90ffd006
Ishaan Virmani <ivirmani@gmail.com>
2019-12-21 14:36:59 +0530

    Update Segment.java

--- Prog8/src/prog8/Segment.java
+++ Prog8/src/prog8/Segment.java
@@ -1,8 +1,8 @@
 package prog8;
 
 public class Segment extends Circle {
-	float segmentArea(float r,float a,float h) {
-		return (float) ((float) Math.pow(r,2)* Math.pow(((r-h)/r)-((r-h) * (2*r*h-Math.pow(h,2))),1/2));
-		
+	float segmentArea(float r, float a, float h) {
+		return (float) ((float) Math.pow(r, 2)
+				* Math.pow(((r - h) / r) - ((r - h) * (2 * r * h - Math.pow(h, 2))), 1 / 2));
 	}
 }
